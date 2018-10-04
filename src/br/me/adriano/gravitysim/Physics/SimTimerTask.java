baf6b95d4d2673252164d63df868c714b882aee6
Adriano Junior <jr.adrianogm@gmail.com>
2018-10-04 15:46:49 -0300

    TimerTask form SimTime class

---
+++ src/br/me/adriano/gravitysim/Physics/SimTimerTask.java
@@ -0,0 +1,27 @@
+package br.me.adriano.gravitysim.Physics;
+
+import java.util.TimerTask;
+
+public class SimTimerTask extends TimerTask{
+	SimTimer timer;
+	double time=0.0;
+	boolean paused = false;
+	boolean changed = false;
+	
+	public SimTimerTask(){
+	}
+	
+	public void setTimer(SimTimer _timer){
+		timer = _timer;
+	}
+	
+	public void run() {
+		if(!paused){
+			System.out.println(time + " -  YEY!");
+			//timer.cancel();
+			//if(time == 5 && !changed) timer.changeInterval(3,time);
+			time++;
+			TimeObject.run();
+		}
+	}
+}
